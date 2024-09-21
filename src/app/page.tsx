6c93501c4fef04af357be1be36851943f98433bf
Naveen Thurimerla <n4endev@gmail.com>
2024-09-21 14:01:24 +0200

    setup basic project layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,138 +1,61 @@
 import Link from "next/link";
-import {
-  SiReact,
-  SiNextdotjs,
-  SiTailwindcss,
-  SiTypescript,
-  SiDocker,
-  SiGit,
-  SiGithub,
-  SiVisualstudiocode,
-  SiJest,
-  SiPostman,
-  SiCypress,
-  SiMocha,
-  SiSelenium,
-  SiCircleci,
-  SiTravisci,
-  SiJenkins,
-  SiSonarqube,
-  SiAzurepipelines,
-} from "react-icons/si";
+import tools from "./toolsData";
+import Image from "next/image";
 
 // Tool types
 interface Tool {
   name: string;
   icon: React.ElementType;
+  url: string;
 }
 
-// Icons for Development Tools
-const developmentTools: Tool[] = [
-  { name: "React", icon: SiReact },
-  { name: "Next.js", icon: SiNextdotjs },
-  { name: "TailwindCSS", icon: SiTailwindcss },
-  { name: "TypeScript", icon: SiTypescript },
-  { name: "Docker", icon: SiDocker },
-  { name: "Git", icon: SiGit },
-  { name: "GitHub", icon: SiGithub },
-  { name: "VS Code", icon: SiVisualstudiocode },
-  { name: "Postman", icon: SiPostman },
-  { name: "Cypress", icon: SiCypress },
-];
+const ToolCard = ({ name, icon: Icon, url }: Tool) => (
+  <Link
+    href={url}
+    target="_blank"
+    className="flex flex-col items-center justify-center p-2 h-full hover:opacity-75"
+  >
+    <Icon className="w-8 h-8 text-black-500 dark:text-white" />
+    <span className="mt-1 text-center text-sm dark:text-white">{name}</span>
+  </Link>
+);
 
-// Icons for Testing Tools
-const testingTools: Tool[] = [
-  { name: "Jest", icon: SiJest },
-  { name: "Postman", icon: SiPostman },
-  { name: "Cypress", icon: SiCypress },
-  { name: "Mocha", icon: SiMocha },
-  { name: "Selenium", icon: SiSelenium },
-  { name: "CircleCI", icon: SiCircleci },
-  { name: "TravisCI", icon: SiTravisci },
-  { name: "Jenkins", icon: SiJenkins },
-  { name: "SonarQube", icon: SiSonarqube },
-  { name: "Azure Pipelines", icon: SiAzurepipelines },
-];
+const ToolSection = ({ title, tools }: { title: string; tools: Tool[] }) => (
+  <div className="flex flex-col items-center">
+    <h2 className="text-2xl font-semibold mb-3 dark:text-white">{title}</h2>
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-2">
+      {tools.map((tool) => (
+        <ToolCard
+          key={tool.name}
+          name={tool.name}
+          icon={tool.icon}
+          url={tool.url}
+        />
+      ))}
+    </div>
+  </div>
+);
 
 export default function Pager() {
-  const ToolCard = ({
-    name,
-    Icon,
-    colorClass,
-  }: {
-    name: string;
-    Icon: React.ElementType;
-    colorClass: string;
-  }) => (
-    <div className="flex flex-col items-center p-2">
-      <Icon className={`w-8 h-8 ${colorClass} dark:text-white`} />
-      <span className="mt-1 text-center text-sm dark:text-white">{name}</span>
-    </div>
-  );
-
   return (
     <div>
       {/* Header Section */}
-      <header className="flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-800 shadow-md">
-        {/* Logo */}
+      <header className="flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-800 shadow-md rounded-lg">
         <Link href="/" className="flex items-center">
-          <span className="text-xl font-semibold dark:text-white">oTen</span>
+          <Image src="/logo.svg" alt="oTen Logo" className="w-10 h-10 mr-2 filter dark:invert" width={10} height={10} />
+          <span className="text-3xl font-extrabold text-black-600 dark:text-black-400 transition-transform transform hover:scale-105">
+            oTen
+          </span>
         </Link>
       </header>
 
       {/* Body Section */}
       <main className="container mx-auto p-4">
-        {/* Tools Section */}
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-2 gap-8">
-          {/* Development Tools */}
-          <div>
-            <h2 className="text-2xl font-semibold mb-3 dark:text-white">
-              Development Tools
-            </h2>
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-2">
-              {developmentTools.map((tool) => (
-                <ToolCard
-                  key={tool.name}
-                  name={tool.name}
-                  Icon={tool.icon}
-                  colorClass="text-black-500"
-                />
-              ))}
-            </div>
-          </div>
-
-          <div>
-            <h2 className="text-2xl font-semibold mb-3 dark:text-white">
-              Development Tools
-            </h2>
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-2">
-              {developmentTools.map((tool) => (
-                <ToolCard
-                  key={tool.name}
-                  name={tool.name}
-                  Icon={tool.icon}
-                  colorClass="text-black-500"
-                />
-              ))}
-            </div>
-          </div>
-
-          {/* Testing Tools */}
-          <div>
-            <h2 className="text-2xl font-semibold mb-3 dark:text-white">
-              Testing Tools
-            </h2>
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-2">
-              {testingTools.map((tool) => (
-                <ToolCard
-                  key={tool.name}
-                  name={tool.name}
-                  Icon={tool.icon}
-                  colorClass="text-black-500"
-                />
-              ))}
-            </div>
-          </div>
+          <ToolSection title="Development" tools={tools.development} />
+          <ToolSection title="Testing" tools={tools.testing} />
+          <ToolSection title="Placeholder2" tools={tools.development} />
+          <ToolSection title="Placeholder3" tools={tools.testing} />
         </div>
       </main>
     </div>
