99250e36f8a94ef8e0adb5905c3234b27db6b493
Naveen Thurimerla <n4endev@gmail.com>
2024-09-21 08:53:24 +0200

    :sparkles: Init project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      oTen - Your guide to the top 10 open-source tools in every stage of the software development lifecycle. Explore the best free tools for DevOps, data engineering, development, and more—all in one place.
+      </main>
+    </div>
+  );
+}
